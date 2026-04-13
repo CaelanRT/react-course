9622345ae9919685d96f0e8811b6174bb317bff2
Caelan Roberge-Toll <caelanrt15@gmail.com>
2026-04-12 21:56:00 -0400

    going in on this

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,3 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 function Greeting() {
-  return <h2>My first Component</h2>;
+  return <h2>hello world</h2>;
 }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Greeting />);

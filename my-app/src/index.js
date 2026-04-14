8d739c273c3ad6a2a8d40150646d4e42839f2ee0
Caelan Roberge-Toll <caelanrt15@gmail.com>
2026-04-13 22:41:57 -0400

    learning CSS, props and iterating over components dynamically based on data

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,70 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
-function Greeting() {
-  return <h2>hello world</h2>;
-}
+// const firstBook = {
+//   author: "Andy Weir",
+//   title: "Project Hail Mary",
+//   image:
+//     "https://images-na.ssl-images-amazon.com/images/I/91ENQs2KLAL._AC_UL600_SR600,400_.jpg",
+//   alt: "Project Hail Mary",
+// };
+
+// const secondBook = {
+//   author: "Freida McFadden",
+//   title: "The Dinner Party",
+//   image:
+//     "https://images-na.ssl-images-amazon.com/images/I/71LtTkRdVzL._AC_UL600_SR600,400_.jpg",
+//   alt: "The Dinner Party",
+// };
+
+const books = [
+  {
+    author: "Andy Weir",
+    title: "Project Hail Mary",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/91ENQs2KLAL._AC_UL600_SR600,400_.jpg",
+    alt: "Project Hail Mary",
+  },
+  {
+    author: "Freida McFadden",
+    title: "The Dinner Party",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/71LtTkRdVzL._AC_UL600_SR600,400_.jpg",
+    alt: "The Dinner Party",
+  },
+];
+
+const BookList = () => {
+  return (
+    <section className="booklist">
+      {books.map((book) => {
+        return (
+          <Book
+            image={book.image}
+            title={book.title}
+            author={book.author}
+            alt={book.alt}
+          />
+        );
+      })}
+    </section>
+  );
+};
+
+const Book = (props) => {
+  const { image, alt, title, author } = props;
+
+  return (
+    <article className="book">
+      <img src={image} alt={alt} />
+      <h2>{title}</h2>
+      <h4>{author.toUpperCase()}</h4>
+      {props.children}
+    </article>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Greeting />);
+root.render(<BookList />);

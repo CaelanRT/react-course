a05f760b87f98d15f72832341b80d08454bc6507
Caelan Roberge-Toll <caelanrt15@gmail.com>
2026-05-09 15:32:03 -0400

    fixed about component

--- react-course-v3-main/02-backroads-project/backroads-app/src/App.js
+++ react-course-v3-main/02-backroads-project/backroads-app/src/App.js
@@ -3,12 +3,14 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Services from "./components/Services";
 import Tours from "./components/Tours";
+import About from "./components/About";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <About />
       <Services />
       <Tours />
       <Footer />

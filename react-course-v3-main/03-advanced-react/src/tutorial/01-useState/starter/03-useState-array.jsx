8376ad0ff20dc70da1c0381972ff2e3fec6e4f21
Caelan Roberge-Toll <caelanrt15@gmail.com>
2026-05-11 22:09:05 -0400

    learning fun things about the web and remembering others!

--- react-course-v3-main/03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ react-course-v3-main/03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,36 @@
+import React from "react";
+import { data } from "../../../data";
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data);
+
+  // functions need to be inside the outer function for scope!
+  const removeSinglePerson = (id) => {
+    // filter is looking for a flag and returns a shallow copy of the array where everything matches!!
+    const newPeople = people.filter((person) => person.id !== id);
+
+    setPeople(newPeople);
+  };
+
+  const clearAllItems = () => {
+    setPeople([]);
+  };
+
+  return (
+    <div>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id}>
+            <h3>{name}</h3>
+            {/* this is how you send a parameter to another function from the onClick!! */}
+            <button onClick={() => removeSinglePerson(id)}>remove</button>
+          </div>
+        );
+      })}
+      <button onClick={clearAllItems}>Clear Items</button>
+    </div>
+  );
 };
 
 export default UseStateArray;

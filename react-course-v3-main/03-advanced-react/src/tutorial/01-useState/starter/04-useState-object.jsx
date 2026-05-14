a357f6f901356c5e3ac5ae07445ec61d73f7d668
Caelan Roberge-Toll <caelanrt15@gmail.com>
2026-05-14 16:50:17 -0400

    useState object

--- react-course-v3-main/03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ react-course-v3-main/03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,22 +1,30 @@
 import { useState } from "react";
 
 const UseStateObject = () => {
-  const [name, setName] = useState("peter");
-  const [age, setAge] = useState(26);
-  const [hobby, setHobby] = useState("read books");
+  // can refactor some of these lines into a state object
+  // const [name, setName] = useState("peter");
+  // const [age, setAge] = useState(26);
+  // const [hobby, setHobby] = useState("read books");
+
+  const [person, setPerson] = useState({
+    name: "peter",
+    age: 24,
+    hobby: "read books",
+  });
 
   const handleClick = () => {
-    setName("john");
-    setAge("28");
-    setHobby("scream at computer");
+    setPerson({ name: "john", age: 28, hobby: "scream at computer" });
+
+    // can also shallow copy the person object to modify only 1 value in the object
+    // setPerson({ ...person, name: "john" });
   };
 
   return (
     <>
       <h2>useState object example</h2>
-      <h4>{name}</h4>
-      <h4>{age}</h4>
-      <h4>Enjoys: {hobby}</h4>
+      <h4>{person.name}</h4>
+      <h4>{person.age}</h4>
+      <h4>Enjoys: {person.hobby}</h4>
       <button onClick={handleClick}>Show John</button>
     </>
   );

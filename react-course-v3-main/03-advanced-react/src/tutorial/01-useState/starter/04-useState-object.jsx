7190fd4ef368deb4b985902a0a4e39ad183e8bef
Caelan Roberge-Toll <caelanrt15@gmail.com>
2026-05-14 16:40:47 -0400

    doing some batching and state objects

--- react-course-v3-main/03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ react-course-v3-main/03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,25 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [name, setName] = useState("peter");
+  const [age, setAge] = useState(26);
+  const [hobby, setHobby] = useState("read books");
+
+  const handleClick = () => {
+    setName("john");
+    setAge("28");
+    setHobby("scream at computer");
+  };
+
+  return (
+    <>
+      <h2>useState object example</h2>
+      <h4>{name}</h4>
+      <h4>{age}</h4>
+      <h4>Enjoys: {hobby}</h4>
+      <button onClick={handleClick}>Show John</button>
+    </>
+  );
 };
 
 export default UseStateObject;

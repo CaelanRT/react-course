9e399da7f27816eb76dd5c5dfda30530a384f26b
Caelan Roberge-Toll <caelanrt15@gmail.com>
2026-05-14 17:09:13 -0400

    finished use state!

--- react-course-v3-main/03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ react-course-v3-main/03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,36 @@
+import { useState } from "react";
+
+// the state value does not synchronously update, when you click the button your displayed
+// value will be 1 higher than the one you have stored in your state
+// to get it to update, you can pass the previous state into your setState function and you're returning the new state
+// your state value
+// you must return something otherwise undefined!
 const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
+  const [value, setValue] = useState(0);
+
+  // const handleClick = () => {
+  //   setValue((currentState) => {
+  //     console.log(currentState);
+  //     const newState = currentState + 1;
+  //     return newState;
+  //   });
+  //   console.log(value);
+  // };
+
+  const handleClick = () => {
+    setTimeout(() => {
+      setValue((currentState) => {
+        return currentState + 1;
+      });
+    }, 3000);
+  };
+
+  return (
+    <div>
+      <h1>{value}</h1>
+      <button onClick={handleClick}>click me</button>
+    </div>
+  );
 };
 
 export default UseStateGotcha;

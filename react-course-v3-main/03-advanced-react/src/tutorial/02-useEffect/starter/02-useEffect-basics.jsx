1e30a8524f69b7e3fb5db94324b04f6a90346ab1
Caelan Roberge-Toll <caelanrt15@gmail.com>
2026-05-14 22:49:13 -0400

    useEffect

--- react-course-v3-main/03-advanced-react/src/tutorial/02-useEffect/starter/02-useEffect-basics.jsx
+++ react-course-v3-main/03-advanced-react/src/tutorial/02-useEffect/starter/02-useEffect-basics.jsx
@@ -1,17 +1,24 @@
-import { useState } from 'react';
+import { useState, useEffect } from "react";
 
+// functions declared in the body of a component are invoked on every re-render
 const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
   const sayHello = () => {
-    console.log('hello there');
+    console.log("hello there");
   };
 
   sayHello();
 
+  // cannot make useEffect async, you can create a function inside of useEffect that is async but just don't make
+  // the callback inside the useEffect invocation async
+  useEffect(() => {
+    console.log("hello from useEffect");
+  }, []);
+
   return (
     <div>
       <h1>value : {value}</h1>
-      <button className='btn' onClick={() => setValue(value + 1)}>
+      <button className="btn" onClick={() => setValue(value + 1)}>
         click me
       </button>
     </div>

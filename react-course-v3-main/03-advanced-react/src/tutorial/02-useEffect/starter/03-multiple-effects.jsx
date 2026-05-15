0a746f74a06f4fc4c6c869787f1a0b980471eb75
Caelan Roberge-Toll <caelanrt15@gmail.com>
2026-05-14 23:20:14 -0400

    learned about fetch and how to use useEffect

--- react-course-v3-main/03-advanced-react/src/tutorial/02-useEffect/starter/03-multiple-effects.jsx
+++ react-course-v3-main/03-advanced-react/src/tutorial/02-useEffect/starter/03-multiple-effects.jsx
@@ -1,24 +1,28 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 const MultipleEffects = () => {
   const [value, setValue] = useState(0);
   const [secondValue, setSecondValue] = useState(0);
 
-  // useEffect(() => {
-  //   console.log('hello from first useEffect');
-  // }, []);
+  // useEffect is essentially you setting up conditionally what you want to have run based on the dependency array
+  // if you want to have it not run anything, leave blank
+  // when you update this value passed in, useEffect is called
+  // essentially a switch statement if you want to set up things conditionally
+  useEffect(() => {
+    console.log("hello from first useEffect");
+  }, []);
 
-  // useEffect(() => {
-  //   console.log('hello from second useEffect');
-  // }, []);
+  useEffect(() => {
+    console.log("hello from second useEffect");
+  }, [secondValue]);
   return (
     <div>
       <h1>value : {value}</h1>
-      <button className='btn' onClick={() => setValue(value + 1)}>
+      <button className="btn" onClick={() => setValue(value + 1)}>
         value
       </button>
       <h1>second value : {secondValue}</h1>
-      <button className='btn' onClick={() => setSecondValue(secondValue + 1)}>
+      <button className="btn" onClick={() => setSecondValue(secondValue + 1)}>
         second value
       </button>
     </div>
--- react-course-v3-main/03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ react-course-v3-main/03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,39 @@
-const url = 'https://api.github.com/users';
+import { useEffect, useState } from "react";
+
+const url = "https://api.github.com/users";
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
+  const [users, setUsers] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setUsers(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <h2>fetch data example</h2>
+      <h4>Github Users</h4>
+      {users.map((user) => {
+        const { login, avatar_url, html_url, id } = user;
+        return (
+          <div key={id}>
+            <img src={avatar_url} alt="" style={{ width: "200px" }} />
+            <p>{login}</p>
+            <a href={html_url}>Profile</a>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 export default FetchData;

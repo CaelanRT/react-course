20d6d4848ad8ffddc86cfc7a43a5d2cbbb816f4f
Caelan Roberge-Toll <caelanrt15@gmail.com>
2026-05-22 13:57:41 -0400

    still more conditional rendering

--- react-course-v3-main/03-advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ react-course-v3-main/03-advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,18 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
+  const [first, setFirst] = useState("");
+  const [second, setSecond] = useState("susan");
+
+  return (
+    <>
+      <div>
+        <h4>Falsy OR : {first || "hello world"}</h4>
+        <h4>Falsy AND : {first && "hello world"}</h4>
+        <h4>Truly OR : {second || "hello world"}</h4>
+        <h4>Truthy AND : {second && "hello world"}</h4>
+      </div>
+    </>
+  );
 };
 export default ShortCircuitOverview;
--- react-course-v3-main/03-advanced-react/src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
+++ react-course-v3-main/03-advanced-react/src/tutorial/03-conditional-rendering/starter/05-short-circuit-examples.jsx
@@ -1,14 +1,41 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const ShortCircuitExamples = () => {
   // falsy
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   // truthy
-  const [name, setName] = useState('susan');
-  const [user, setUser] = useState({ name: 'john' });
+  const [name, setName] = useState("susan");
+  const [user, setUser] = useState({ name: "john" });
   const [isEditing, setIsEditing] = useState(false);
 
-  return <h2>short circuit - examples</h2>;
+  return (
+    <div>
+      {/* - if the value is there, display the value or show the dummy value
+          - we use || to control whether we show a default value or the data
+          - we use && to control if we are showing additional elements or data if a condition is true or nothing if not
+          - this will be dynamic
+      */}
+
+      <h2>{text || "default value"}</h2>
+      {text && (
+        <div>
+          <h2>whatever return</h2>
+          <h2>{name}</h2>
+        </div>
+      )}
+      {/* this is returning a component with a prop! */}
+      {user && <SomeComponent name={user.name} />}
+    </div>
+  );
+};
+
+const SomeComponent = ({ name }) => {
+  return (
+    <div>
+      <h2>whatever return</h2>
+      <h2>{name}</h2>
+    </div>
+  );
 };
 
 export default ShortCircuitExamples;

773d5cf651c7df26964be23aa30fff42064a65eb
Caelan Roberge-Toll <caelanrt15@gmail.com>
2026-05-22 14:26:04 -0400

    crushed the challenge once again

--- react-course-v3-main/03-advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ react-course-v3-main/03-advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,32 @@
+import { useState } from "react";
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+  const [user, setUser] = useState(null);
+
+  const login = () => {
+    // normally this is where you connect to the db or api
+    setUser({ name: "Billy" });
+  };
+
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <>
+      {user ? (
+        <div>
+          <h4>Hello there: {user.name}</h4>
+          <button onClick={logout}>Logout</button>
+        </div>
+      ) : (
+        <div>
+          <h4>Please log in</h4>
+          <button onClick={login}>Login</button>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default UserChallenge;

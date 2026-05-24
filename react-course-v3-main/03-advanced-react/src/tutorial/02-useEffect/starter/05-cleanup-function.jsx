e296f12b19ebc7835d4ec98fdd0bec319c41d266
Caelan Roberge-Toll <caelanrt15@gmail.com>
2026-05-24 15:41:41 -0400

    learning about cleanup functions

--- react-course-v3-main/03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ react-course-v3-main/03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,31 @@
+import { useEffect, useState } from "react";
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <>
+      <h2>Cleanup Function</h2>
+      <button
+        onClick={() => {
+          setToggle(!toggle);
+        }}
+      >
+        Toggle
+      </button>
+      {toggle && <SecondComponent />}
+    </>
+  );
+};
+
+// use effect is firing on every click here, use effect is running every time even though the dependency array is empty
+// this is because it is rerendering due to conditional rendering
+const SecondComponent = () => {
+  useEffect(() => {
+    console.log("here");
+  }, []);
+
+  return <h3>Second component</h3>;
 };
 
 export default CleanupFunction;

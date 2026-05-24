4af0afb655dff714ac92240ad2a4bb6de8f2763e
Caelan Roberge-Toll <caelanrt15@gmail.com>
2026-05-24 15:55:03 -0400

    done that part, moving to projects

--- react-course-v3-main/03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ react-course-v3-main/03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -20,9 +20,15 @@ const CleanupFunction = () => {
 
 // use effect is firing on every click here, use effect is running every time even though the dependency array is empty
 // this is because it is rerendering due to conditional rendering
+// write a cleanup function, essentially just cleaning up the functionality you're using
+// if you're subscribing to a service in useEffect, you want to unsubscribe in your cleanup
 const SecondComponent = () => {
   useEffect(() => {
     console.log("here");
+    setInterval(() => {
+      console.log("hello from interval");
+    }, 1000);
+    return () => {};
   }, []);
 
   return <h3>Second component</h3>;

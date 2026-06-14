98e509291f48d2e05cba8661e526630e51ef1c95
Caelan Roberge-Toll <caelanrt15@gmail.com>
2026-06-13 22:17:30 -0400

    did the extra challenge, honestly feels really good to do some stuff!

--- react-course-v3-main/03-advanced-react/src/tutorial/06-forms/starter/01-controlled-inputs.jsx
+++ react-course-v3-main/03-advanced-react/src/tutorial/06-forms/starter/01-controlled-inputs.jsx
@@ -1,4 +1,58 @@
+import { useState } from "react";
+
 const ControlledInputs = () => {
-  return <h2>Controlled Inputs</h2>;
+  // were essentially trying to save whatever gets typed into state, and then when you need to post things you send it to server by grabbing
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  // const handleChange = (e) => {
+
+  //   setName(e.target.value);
+  // };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // this is where you do something to with the values? send to server
+    console.log(name, email);
+  };
+
+  return (
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <h4>controlled inputs</h4>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            Name:
+          </label>
+          {/* need to set up the value for the state value and on change otherwise it won't work */}
+          <input
+            type="text"
+            name=""
+            id="name"
+            className="form-input"
+            value={name}
+            // can use handleChange just as is here
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="form-row">
+          <label htmlFor="email" className="form-label">
+            Email:
+          </label>
+          <input
+            type="text"
+            name=""
+            id="email"
+            className="form-input"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+      </form>
+    </>
+  );
 };
 export default ControlledInputs;
--- react-course-v3-main/03-advanced-react/src/tutorial/06-forms/starter/02-user-challenge.jsx
+++ react-course-v3-main/03-advanced-react/src/tutorial/06-forms/starter/02-user-challenge.jsx
@@ -1,20 +1,54 @@
+import { useState } from "react";
+import { data } from "../../../data";
+
 const UserChallenge = () => {
+  const [name, setName] = useState("");
+  const [users, setUsers] = useState(data);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setUsers((prevUsers) => [
+      ...prevUsers,
+      { id: prevUsers.length + 1, name: name },
+    ]);
+  };
+
   return (
     <div>
-      <form className='form'>
+      <form className="form" onSubmit={handleSubmit}>
         <h4>Add User</h4>
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
             name
           </label>
-          <input type='text' className='form-input' id='name' />
+          <input
+            type="text"
+            className="form-input"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
 
-        <button type='submit' className='btn btn-block'>
+        <button type="submit" className="btn btn-block">
           submit
         </button>
       </form>
-      {/* render users below */}
+      {users.map((user) => {
+        return (
+          <>
+            <h4 key={user.id}>{user.name}</h4>
+            <button
+              onClick={() =>
+                setUsers(users.filter((innerUser) => user.id !== innerUser.id))
+              }
+            >
+              delete
+            </button>
+          </>
+        );
+      })}
     </div>
   );
 };
